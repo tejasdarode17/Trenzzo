cf34fee56c4060eb347ffe197b1884f0f4e1ce54
Tejas Darode <tejasdarode17@gmail.com>
2026-01-31 17:00:37 +0530

    deploy attempt 4

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,8 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'sonner'
 import { Provider } from 'react-redux'
-import store from './redux/Store'
-
+import store from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <>
@@ -13,5 +12,5 @@ createRoot(document.getElementById('root')).render(
       <App />
     </Provider>
   </>
-
 )
+
